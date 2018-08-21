aeadefda953e1cf55525fdd284375734d5a41e71
Bryan Stitt <bryan@stitthappens.com>
2018-08-20 22:21:13 -0700

    simplify

--- el_sequencer/el_sequencer.ino
+++ el_sequencer/el_sequencer.ino
@@ -1,6 +1,8 @@
+// TODO: next time, I think we should just use the shield instead of having two MCUs
+
 #include <SoftwareSerial.h>
 
-// TODO: this isn't working. i think our cables might be too long. nothing is coming in over this
+// TODO: why didn't A6 and A7 work?
 #define SEQUENCER_RX A4  // green connected to Teensy's TX (1)
 #define SEQUENCER_TX A3  // yellow connected to Teensy's RX (0)
 SoftwareSerial mySerial(SEQUENCER_RX, SEQUENCER_TX);
@@ -22,11 +24,6 @@ unsigned char data = 0;
 // track the millis() of when we last received a message over serial
 unsigned long lastData = 0;
 
-// the teensy already has logic for keeping the lights from flickering. this is really just a backup
-// TODO: delete this logic entirely? having it just on the teensy side should be fine
-unsigned int minOnMs = 11.5 * 2;
-unsigned long turnOffMsArray[NUM_OUTPUTS];
-
 void setup() {
   Serial.begin(115200);
 
@@ -34,6 +31,11 @@ void setup() {
 
   for (int i = 0; i < NUM_OUTPUTS; i++) {
     pinMode(outputPins[i], OUTPUT);
+
+    // blink the wire once
+    digitalWrite(outputPins[i], HIGH);
+    delay(200);
+    digitalWrite(outputPins[i], LOW);
   }
 
   Serial.println("Started...");
@@ -45,57 +47,40 @@ void loop() {
 
     lastData = millis();
 
+    // turn the outputs on or off depending on the data received
     for (int i = 0; i < NUM_OUTPUTS; i++) {
       if (bitRead(data, i) == 1) {
-        // TODO: make sure that we don't turn on more than 6 lights
-        // TODO: I think we need to loop once to check if the lights should turn off and then loop again so that numOn is correct
-        digitalWrite(outputPins[i], HIGH);
-
-        // make sure we stay on for a minimum amount of time
-        turnOffMsArray[i] = millis() + minOnMs;
         Serial.print("| 1 ");
+        digitalWrite(outputPins[i], HIGH);
       } else {
-        if (millis() < turnOffMsArray[i]) {
-          // the output has not been on for long enough. leave it on.
-          Serial.print("| 0 ");
-        } else {
-          // the output has been on for at least minOnMs and is quiet now. turn it off
-          digitalWrite(outputPins[i], LOW);
-          Serial.print("|   ");
-        }
+        Serial.print("|   ");
+        digitalWrite(outputPins[i], LOW);
       }
     }
     Serial.println("|");
   }
 
-  if (millis() - lastData < 5000) {
-    // while we wait for new data from the Teensy
-    // check to see if we should turn anything off
-    for (int i = 0; i < NUM_OUTPUTS; i++) {
-      if (bitRead(data, i) == 0) {
-        // this output should be off
-        if (millis() < turnOffMsArray[i]) {
-          // the output has not been on for long enough to prevent flickering. leave it on.
-        } else {
-          // the output has been on for at least minOnMs and is quiet now. turn it off
-          // we do this every iteration to be as responsive as possible
-          digitalWrite(outputPins[i], LOW);
-
-          // flip the bit so we don't bother checking this output again
-          bitSet(data, i);
-        }
+  if (millis() - lastData > 5000) {
+    // it's been over 5 seconds and we haven't received any data, blink fun patterns
+    while (1) {
+      if (mySerial.available()) {
+        // the serial connection has returned!
+        // i doubt this will ever happen, but it doesn't really hurt to check
+        break;
       }
+
+      blinkPatterns();
     }
-  } else {
-    // it's been over 5 seconds and we haven't received any data, blink
-
-    Serial.println("| BLINKY |");
-
-    // TODO: actually blink a cool pattern
-    digitalWrite(outputPins[0], HIGH);
-    digitalWrite(outputPins[2], HIGH);
-    digitalWrite(outputPins[4], HIGH);
-    digitalWrite(outputPins[6], HIGH);
-    delay(1000);
   }
+}
+
+void blinkPatterns() {
+  Serial.println("| BLINKY |");
+
+  // TODO: actually blink a cool pattern
+  digitalWrite(outputPins[0], HIGH);
+  digitalWrite(outputPins[2], HIGH);
+  digitalWrite(outputPins[4], HIGH);
+  digitalWrite(outputPins[6], HIGH);
+  delay(1000);
 }
